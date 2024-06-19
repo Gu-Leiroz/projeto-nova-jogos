157c162542860574469bc464e7bd962a1f2b23d6
Gustavo Leiroz <gustavoleiroz35@gmai.com>
2024-06-18 22:32:22 -0300

    Commit front-end

---
+++ projetoNovaJogos/projeto-nova-jogos/client/src/pages/Login/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './styles.css';
+
+import logoImg from '../../assets/cogumelo.png'
+import padlock from '../../assets/botao-de-login.png'
+
+export default function Login() {
+    return (
+
+        <div className="login-conteiner">
+
+            <section className="form">
+                <img src={logoImg} alt="Logo" />
+                <form>
+                    <h1>Acesse sua conta</h1>
+                    <input placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+
+
+                    <button className="button" type="submit">Login</button>
+                </form>
+
+            </section>
+[
+            
+
+        </div>
+       
+
+    )
+}
